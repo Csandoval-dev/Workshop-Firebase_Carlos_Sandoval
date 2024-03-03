25868ce014c384471cee7dd9f7af7a575b2ce6a1
Csandoval-dev <156733491+Csandoval-dev@users.noreply.github.com>
2024-03-03 15:14:34 -0600

    Escribir mensajes en Firestore

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -1,10 +1,14 @@
+import 'dart:async';                                     // new
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider, PhoneAuthProvider;
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
+import 'guest_book_message.dart';                        // new
 
 class ApplicationState extends ChangeNotifier {
 
